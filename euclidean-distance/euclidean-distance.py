9659c7c9a961a53b11a87f7a722ea37eb248b55c
Mohamed AL_MANSOURY <99142119+mohamedalmansoury@users.noreply.github.com>
2026-01-08 20:05:50 +0200

    Update solution for euclidean-distance

--- euclidean-distance/euclidean-distance.py
+++ euclidean-distance/euclidean-distance.py
@@ -8,4 +8,4 @@ def euclidean_distance(x, y):
     x = np.asarray(x, dtype=float)
     y = np.asarray(y, dtype=float)
     
-    return float(np.sqrt(np.sum((x - y) ** 2)))
+    return float(np.linalg.norm(x - y) )
